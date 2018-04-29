9a7681aadb9fff9f70f0c78fb734a231a015c40a
ruiqin <rli@rlis-MacBook-Pro.local>
2018-04-29 18:19:21 -0400

    turn into gradle project

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+public class DemoTest {
+  @Test
+  public void test() {
+    System.out.println("demo test");
+
+    assert true;
+
+  }
+
+}
